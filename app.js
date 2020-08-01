b8603306f3fc137ea5c7e50af419fe4fa52f668b
Jeisson Santiesteban <jeisson.santiesteban@uptc.edu.co>
2020-08-01 17:42:29 -0500

    Añadida la funcionalidad de reproducir musica

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bot = new Discord.Client();
 bot.on("ready", () => {
   console.log("Bot is ready");
 });
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   if (msg.content === "!pp") {
     let size = Math.floor(Math.random() * 31 + 1);
     if (msg.author.id === "299230070655025154")
@@ -17,6 +17,19 @@ bot.on("message", (msg) => {
     else if (size === 30) text = "papi :3";
     else text = "JAJAJAJAJA";
     msg.reply(`Te mide ${size} ${text}`);
+  }
+  if (msg.content.split(" ")[0] === "!join") {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (msg.member.voice.channel) {
+      const connection = await msg.member.voice.channel.join();
+      const ytdl = require("ytdl-core");
+      if (msg.content.split(" ")[1] !== undefined)
+        connection.play(
+          ytdl(msg.content.split(" ")[1], { filter: "audioonly" })
+        );
+    } else {
+      msg.reply("You need to join a voice channel first!");
+    }
   }
 });
 bot.login(process.env.BOT);
